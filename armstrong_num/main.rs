d83a53e0e2a580a45eebe07d2c7435f113449d17
knightron0 <sarthak.robo@gmail.com>
2019-12-05 08:16:08 +0530

    Fix bug

--- armstrong_num/main.rs
+++ armstrong_num/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 fn main(){
-	let mut input = String::new();
+    let mut input = String::new();
     println!("Enter a number: ");
     io::stdin().read_line(&mut input).unwrap();
     let mut n: i32 = input.trim().parse().unwrap();
